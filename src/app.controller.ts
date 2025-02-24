6134361b664f4d154c5379d18eafadcd2e4ec26c
Flávio Peres <flaviopc2@gmail.com>
2025-02-24 14:29:20 -0300

    alterando o callback para funcionar em produção 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'API está funcionando!';
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import axios from 'axios'
 
 @Injectable()
-export class AppService implements OnModuleInit {
-  async onModuleInit() {
-    setInterval(async () => {
-      try {
-        // Enviar um "ping" para a rota /health para garantir que a API está ativa
-        await axios.get('https://backend-beatloop-nest.onrender.com');
-        console.log('Ping enviado para manter a API ativa');
-      } catch (error) {
-        console.error('Erro ao enviar ping:', error);
-      }
-    }, 1000 * 60 * 13); // A cada 13 minutos
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
   }
 }
